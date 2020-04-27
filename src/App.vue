74e0041378b190ef062faadd062d5d906d696e95
Nelson Atuonwu <nellyatuonuw@bmail.com>
2020-04-27 15:54:57 +0100

    added components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <app-header></app-header>
     <router-view/>
+    <app-footer></app-footer>
   </div>
 </template>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="bg-primary w-full py-4">
+        <div class="container grid grid-rows-2 row-gap-5">
+            <div class="header__content flex justify-between">
+                <div class="header__contect-icon flex justify-center items-center">
+                    <button class="bg-transparent pr-2">
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <b class="text-base uppercase">a - z</b>
+                </div>
+                <img :src="logo" alt="" srcset="">
+                <button>
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+            <div class="header__actions grid gap-4 grid-cols-2">
+                <button class="rounded-sm text-white uppercase px-10 py-2 bg-red-500">join now</button>
+                <button class="rounded-sm text-white uppercase px-10 py-2 bg-secondary">login</button>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+    setup() {
+        const logo = computed(() => require('@/assets/logo.svg'))
+        return { logo }
+    }
+}
+</script>
+
+<style>
+
+</style>
